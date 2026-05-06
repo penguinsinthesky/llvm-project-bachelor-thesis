b6bdf6fddc013b495d49eb36ed42d53280f0baa7
penguinsinthesky <github@sebastianclamer.de>
2026-05-06 15:22:24 +0200

    runtime access to custom region information

---
+++ compiler-rt/include/xray/xray_custom_regions.h
@@ -0,0 +1,44 @@
+#ifndef LLVM_XRAY_CUSTOM_REGIONS_H
+#define LLVM_XRAY_CUSTOM_REGIONS_H
+
+#include <cstdint>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// synchronize with llvm::xray::CustomRegionKind !
+enum XRayCustomRegionKind {
+  INLINED_FUNCTION = 1,
+  USER_PLACED = 2,
+
+  /// May represent invalid region IDs
+  INVALID = -1
+};
+
+/**
+ * Returns a string representation of XRayCustomRegionKind as a string
+ * with static lifetime.
+ */
+const char *__xray_custom_region_kind_string(enum XRayCustomRegionKind kind);
+
+/**
+ * Returns a custom region's kind or INVALID if the provided ID is out of
+ * bounds.
+ */
+enum XRayCustomRegionKind __xray_custom_region_get_kind(uint32_t region_id);
+
+/**
+ * Returns a custom region's name or null if the provided ID is out of bounds.
+ */
+const char *__xray_custom_region_get_name(uint32_t region_id);
+
+/// Returns the number of custom regions present in the binary
+// TODO not really, this is currently the number of probes
+uint32_t __xray_get_num_custom_regions();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LLVM_XRAY_CUSTOM_REGIONS_H
---
+++ compiler-rt/lib/xray/xray_custom_regions.cpp
@@ -0,0 +1,78 @@
+#include "xray/xray_custom_regions.h"
+
+#include "sanitizer_common/sanitizer_common.h"
+
+#include <cstddef>
+
+using __sanitizer::Report;
+
+extern "C" {
+
+struct XRayCustomRegionEntry {
+
+  // objects should only live in the .xray_custom_regions sections and thus
+  // should not be copied/moved anywhere
+  XRayCustomRegionEntry(const XRayCustomRegionEntry &other) = delete;
+  XRayCustomRegionEntry(XRayCustomRegionEntry &&other) noexcept = delete;
+  XRayCustomRegionEntry &operator=(const XRayCustomRegionEntry &other) = delete;
+  XRayCustomRegionEntry &
+  operator=(XRayCustomRegionEntry &&other) noexcept = delete;
+
+#if SANITIZER_WORDSIZE == 64
+  uint64_t NameOffset;
+  uint64_t Kind;
+#elif SANITIZER_WORDSIZE == 32
+  uint32_t NameOffset;
+  uint32_t Kind;
+#else
+#error "Unsupported word size."
+#endif
+
+  const char *getName() const {
+    return reinterpret_cast<const char *>(&NameOffset) + NameOffset;
+  }
+};
+
+extern const XRayCustomRegionEntry __start_xray_custom_regions[]
+    __attribute__((weak));
+extern const XRayCustomRegionEntry __stop_xray_custom_regions[]
+    __attribute__((weak));
+// TODO maybe divide by sizeof(entry)
+const static uint32_t NUM_ENTRIES =
+    __stop_xray_custom_regions - __start_xray_custom_regions;
+
+const char *__xray_custom_region_kind_string(const XRayCustomRegionKind kind) {
+  switch (kind) {
+  case INLINED_FUNCTION:
+    return "INLINED_FUNCTION";
+  case USER_PLACED:
+    return "USER_PLACED";
+  case INVALID:
+    return "INVALID";
+  }
+
+  Report("Invalid XRayCustomRegionKind value: %u\n", kind);
+  return nullptr;
+}
+
+XRayCustomRegionKind __xray_custom_region_get_kind(const uint32_t region_id) {
+  if (region_id >= NUM_ENTRIES) {
+    Report("Invalid custom region ID: %u\n", region_id);
+    return INVALID;
+  }
+
+  return static_cast<XRayCustomRegionKind>(
+      __start_xray_custom_regions[region_id].Kind);
+}
+
+const char *__xray_custom_region_get_name(const uint32_t region_id) {
+  if (region_id >= NUM_ENTRIES) {
+    Report("Invalid custom region ID: %u\n", region_id);
+    return nullptr;
+  }
+
+  return __start_xray_custom_regions[region_id].getName();
+}
+
+uint32_t __xray_get_num_custom_regions() { return NUM_ENTRIES; }
+}
