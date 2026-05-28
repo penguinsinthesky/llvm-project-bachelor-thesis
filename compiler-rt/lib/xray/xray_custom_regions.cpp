df9bb1e905b91564c868d5366ef4805e6cbc1383
penguinsinthesky <github@sebastianclamer.de>
2026-05-28 12:35:20 +0200

    XRAY_NEVER_INSTRUMENT attribute for xray custom region library functions

--- compiler-rt/lib/xray/xray_custom_regions.cpp
+++ compiler-rt/lib/xray/xray_custom_regions.cpp
@@ -1,6 +1,8 @@
 #include "xray/xray_custom_regions.h"
 
 #include "sanitizer_common/sanitizer_common.h"
+#include "sanitizer_common/sanitizer_internal_defs.h"
+#include "xray/xray_defs.h"
 
 using __sanitizer::Report;
 
@@ -26,7 +28,7 @@ struct XRayCustomRegionEntry {
 #error "Unsupported word size."
 #endif
 
-  const char *getName() const {
+  const char *getName() const XRAY_NEVER_INSTRUMENT {
     return reinterpret_cast<const char *>(&NameOffset) + NameOffset;
   }
 };
@@ -38,7 +40,8 @@ extern const XRayCustomRegionEntry __stop_xray_custom_regions[]
 const static uint32_t NUM_ENTRIES =
     __stop_xray_custom_regions - __start_xray_custom_regions;
 
-const char *__xray_custom_region_kind_string(const XRayCustomRegionKind kind) {
+const char *__xray_custom_region_kind_string(const XRayCustomRegionKind kind)
+    XRAY_NEVER_INSTRUMENT {
   switch (kind) {
   case INLINED_FUNCTION:
     return "INLINED_FUNCTION";
@@ -52,7 +55,8 @@ const char *__xray_custom_region_kind_string(const XRayCustomRegionKind kind) {
   return nullptr;
 }
 
-XRayCustomRegionKind __xray_custom_region_get_kind(const uint32_t region_id) {
+XRayCustomRegionKind
+__xray_custom_region_get_kind(const uint32_t region_id) XRAY_NEVER_INSTRUMENT {
   if (region_id >= NUM_ENTRIES) {
     Report("Invalid custom region ID: %u\n", region_id);
     return INVALID;
@@ -62,7 +66,8 @@ XRayCustomRegionKind __xray_custom_region_get_kind(const uint32_t region_id) {
       __start_xray_custom_regions[region_id].Kind);
 }
 
-const char *__xray_custom_region_get_name(const uint32_t region_id) {
+const char *
+__xray_custom_region_get_name(const uint32_t region_id) XRAY_NEVER_INSTRUMENT {
   if (region_id >= NUM_ENTRIES) {
     Report("Invalid custom region ID: %u\n", region_id);
     return nullptr;
